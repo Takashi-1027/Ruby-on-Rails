c1f7440827891d9c93ea4b2b0fce3effac6a7ff5
Takashi-1027 <taka19921027@gmail.com>
2021-02-04 15:54:06 +0000

    chapter7 finish

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,27 +1,41 @@
 class BlogsController < ApplicationController
-  def index
-    @blogs = Blog.all
-  end
+    def index
+      @blogs = Blog.all
+    end
 
-  def show
-    @blog = Blog.find(params[:id])  # 記事のIDを取得できる。
-  end
+    def show
+      @blog = Blog.find(params[:id])
+    end
 
-  def new
-    @blog = Blog.new
-  end
+    def new
+      @blog = Blog.new
+    end
 
-  def create
-    blog = Blog.new(blog_params)
-    blog.save
-    redirect_to blogs_path(blog.id)  # 投稿後のリダイレクト先も詳細ページへ設定する
-  end
+    def create
+      blog = Blog.new(blog_params)
+      blog.save
+      redirect_to blogs_path(blog.id)
+    end
 
-  def edit
-  end
+    def edit
+      @blog = Blog.find(params[:id])
+    end
 
-  private
-  def blog_params
-    params.require(:blog).permit(:title, :category, :body)
-  end
+    def update
+      blog = Blog.find(params[:id]) # 投稿データを取得する
+      blog.update(blog_params)
+      redirect_to blog_path(blog)
+    end
+
+    # 削除機能を実装
+    def destroy # destroyアクションを追加する
+      blog = Blog.find(params[:id]) # 投稿データを取得する
+      blog.destroy # 投稿データを削除する
+      redirect_to blogs_path # 投稿の一覧ページへリダイレクトする
+    end
+
+    private
+      def blog_params
+        params.require(:blog).permit(:title, :category, :body)
+      end
 end
